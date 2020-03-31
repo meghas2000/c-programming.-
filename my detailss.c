ff80178c2fad30ef97b8dd17764c90db9810f3cb
meghas2000 <62951003+meghas2000@users.noreply.github.com>
2020-03-31 21:57:32 +0530

    Add files via upload

---
+++ my detailss.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+  printf("Megha S Mohan\nElectronics and Communication\nCEC");
+  return 0;
+  }
---
+++ welcome to learn to code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("welcome to learn to code");
+  return 0;
+}
