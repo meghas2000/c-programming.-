cadba1e050c3b8ac9438fab8d61fffe96c9193d3
meghas2000 <62951003+meghas2000@users.noreply.github.com>
2020-04-02 12:23:01 +0530

    Add files via upload

---
+++ details of a student.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  
+
+  
+
+
+char name[] ="megha";
+char school[]="kv school";
+int age=20;
+int class=10;
+float marks=40.8;
+
+  printf("name of the student is %s\n",name);
+  printf("school of the student is %s\n",school);
+  printf("age of the student is %d\n",age);
+  printf("class of the student is %d\n",class);
+  printf("marks of the studentis %f\n",marks);
+  return 0;
+}
---
+++ details of my pet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{ 
+  char name[]="APPU";
+  int age=2;
+  char favoritefood[]="pedigree";
+  printf("name of the pet is %s\n",name);
+  printf("age of the pet is %d\n",age);
+  printf("favoritefood of the pet is %s\n",favoritefood );
+  return 0;
+}
