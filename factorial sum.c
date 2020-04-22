013b020eb3de924103e1010535c726d84a3017f5
meghas2000 <62951003+meghas2000@users.noreply.github.com>
2020-04-22 20:53:45 +0530

    Add files via upload

---
+++ factorial sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+double sumseries(double);
+ 
+main()
+{
+    double number,sum;
+    sum = sumseries(number);
+    printf("\n Sum of the above series = %lf ", sum);
+}
+ 
+double sumseries(double m)
+{
+    double sum2 = 0, f = 1, i;
+    for (i = 1; i <= 5; i++)
+    {
+        f = f * i;
+        sum2 = sum2 +(i / f);
+    }
+    return(sum2);
+}
